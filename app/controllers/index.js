b907c04234638746a0034fa5710f395ecd64af12
Ian Irving <ian@falsepositives.com>
2017-06-19 16:34:13 -0400

    initial invitation admin page

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -10,14 +10,15 @@ export default Ember.Controller.extend({
   actions: {
 
     saveInvitation () {
-      //      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`)
       const email = this.get('emailAddress')
       const newInvittation = this.store.createRecord('invitation', {
         email: email
       })
-      newInvittation.save()
-      this.set('responseMessage', `Thank you! We've just saved your email address: ${email}`)
-      this.set('emailAddress', '')
+      newInvittation.save().then((response) => {
+        // console.log(`email address ${email} is saved at id ${response.get('id')}`)
+        this.set('responseMessage', `Thank you! We've just saved your email address: ${email}`)
+        this.set('emailAddress', '')
+      })
     }
   }
 
--- app/router.js
+++ app/router.js
@@ -1,14 +1,22 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from 'ember'
+import config from './config/environment'
 
 const Router = Ember.Router.extend({
   location: config.locationType,
   rootURL: config.rootURL
-});
+})
 
-Router.map(function() {
-  this.route('about');
-  this.route('contact');
-});
+Router.map(function () {
+  this.route('about')
+  this.route('contact')
 
-export default Router;
+  this.route('admin', function () {
+    this.route('invitations')
+  })
+
+  this.route('libraries', function () {
+    this.route('new')
+  })
+})
+
+export default Router
