2a5d7b340c5e4a7c7fe6fe40bf4588c8358a13fd
Ian Irving <ian@falsepositives.com>
2017-06-19 15:43:35 -0400

    add invitation model and change controller to save to store there.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -11,7 +11,12 @@ export default Ember.Controller.extend({
 
     saveInvitation () {
       //      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`)
-      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`)
+      const email = this.get('emailAddress')
+      const newInvittation = this.store.createRecord('invitation', {
+        email: email
+      })
+      newInvittation.save()
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${email}`)
       this.set('emailAddress', '')
     }
   }
