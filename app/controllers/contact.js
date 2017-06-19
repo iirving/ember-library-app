a8982d55d9a418b01f088331540ad4d559596d39
Ian Irving <ian@falsepositives.com>
2017-06-19 17:24:21 -0400

    setup contact model, contact admin pages, changes to store contact data

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -11,13 +11,24 @@ export default Ember.Controller.extend({
   actions: {
 
     saveContactMsg () {
-      alert(`Your contact message ${this.get('message')} from the email address of ${this.get('emailAddress')} is in progress.`)
-      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`)
+      const message = this.get('message')
+      const email = this.get('emailAddress')
 
-      this.set('emailAddress', '')
-      this.set('message', '')
+      const newContactMsg = this.store.createRecord('contact', {
+        email: email,
+        message: message
+      })
 
-      this.set('responseMessage', `We got your message and we’ll get in touch soon.`)
+      newContactMsg.save().then((response) => {
+        alert(`Your contact message ${message} from the email address of ${email} is in progress.`)
+
+        this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`)
+
+        this.set('emailAddress', '')
+        this.set('message', '')
+
+        this.set('responseMessage', `We got your message and we’ll get in touch soon.`)
+      })
     }
   }
 
