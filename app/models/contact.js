5bdf1b1a4cf6edc8433f2bbf09f65d9795851702
Ian Irving <ian@falsepositives.com>
2017-06-20 16:25:10 -0400

    refactoring contact

--- app/models/contact.js
+++ app/models/contact.js
@@ -1,6 +1,15 @@
-import DS from 'ember-data';
+import DS from 'ember-data'
+import Ember from 'ember'
 
 export default DS.Model.extend({
+
   email: DS.attr('string'),
-  message: DS.attr('string')
-});
+  message: DS.attr('string'),
+
+  isValidEmailAddress: Ember.computed.match('email', /^.+@.+\..+$/),
+  isValidMessage: Ember.computed.gte('message.length', 5),
+
+  isValid: Ember.computed.and('isValidEmailAddress', 'isValidMessage'),
+  isNotValid: Ember.computed.not('isValid')
+
+})
--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,31 @@
-import Ember from 'ember';
+import Ember from 'ember'
 
 export default Ember.Route.extend({
-});
+
+  model () {
+    return this.store.createRecord('contact')
+  },
+
+  actions: {
+
+    saveContact (newContact) {
+      const that = this // .get('newContact')
+      newContact.save().then((response) => {
+        console.log('response', response)
+        console.log('newContact', newContact)
+        console.log(that)
+        that.set('responseMessage', `Thank you! We've just saved your email address: ${that.get('model.email')}`)
+
+        that.set('model.email', '')
+        that.set('model.message', '')
+
+        // this.transitionTo('contact')
+      })
+    },
+
+    willTransition () {
+      this.controller.get('model').rollbackAttributes()
+    }
+
+  }
+})
