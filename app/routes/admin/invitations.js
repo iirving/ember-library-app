2996be01b1039107c38d97c35fcae5a0e74bfd2d
Ian Irving <ian@falsepositives.com>
2017-07-20 17:37:43 -0400

    add delete actions to admin contacts and admin invites

--- app/routes/admin/invitations.js
+++ app/routes/admin/invitations.js
@@ -3,6 +3,16 @@ import Ember from 'ember'
 export default Ember.Route.extend({
   model () {
     return this.store.findAll('invitation')
+  },
+
+  actions: {
+    deleteInvite (invite) {
+      let confirmation = confirm('Are you sure?')
+
+      if (confirmation) {
+        invite.destroyRecord()
+      }
+    }
   }
 
 })
